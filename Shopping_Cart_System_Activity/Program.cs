2506115b054d51e5e9fc3ebe2748a6ecad6ecaaf
mxrkpxtrxckocxmpx <markpatrickocampo@gmail.com>
2026-04-12 14:28:23 +0800

    3rd Commit: Added an Arrray of 20 Maximum Video Game products/items and a "while loop" to loop mostly the system of my code. Additionally, a "for loop" to display all products. I also added some comments to some line of the code for easier understanding.

--- Shopping_Cart_System_Activity/Program.cs
+++ Shopping_Cart_System_Activity/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Net.Http.Headers;
 
 namespace Shopping_Cart_System_Quiz
 {
     static void Main(string[] args)
     {
+        // Created Array of Products (Video Game Products/Items in particular.)
         Product[] products = new Product[]
         {
             new Product(1, "Resident Evil: Requiem", 3500, 20),
@@ -13,5 +13,22 @@ namespace Shopping_Cart_System_Quiz
             new Product(4, "NBA 2K26,", 3000, 15),
             new Product(5, "Tekken 8", 2500, 10),
         };
+
+        // Created Cart Array for Maximum of 20 Video Game Products/Items only.)
+        CartItem[] cart = new CartItem[20];
+        int cartCount = 0; // This helps keep track of items/products in cart.
+
+        while(true) // This loop continues not until the user stops shopping.
+        {
+            Console.Clear();
+            Console.WriteLine("=== Welcome to Video Game Shop! ===\n");
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i].DisplayProduct(i + 1);
+            }
+
+            
+        }   
     }
 }
